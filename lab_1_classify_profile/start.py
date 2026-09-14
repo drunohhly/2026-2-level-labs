a64636d252ae2d31f484ee57bc183cd05dd45a51
drunohhly <nata26032007@gmail.com>
2026-09-14 21:31:57 +0300

    changed 'start.py'

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,14 +3,15 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-from main import tokenize, remove_stop_words
-from main import remove_stop_words
-from main import calculate_frequencies
-from main import get_top_n_words
-from main import create_language_profile
-from main import check_profile
-from main import detect_language_by_top_n
-from main import detect_language_by_mse
+from main import (
+                calculate_frequencies,
+                check_profile,
+                create_language_profile,
+                detect_language_by_mse,
+                detect_language_by_top_n,
+                get_top_n_words,
+                remove_stop_words,
+                tokenize)
 
 def main() -> None:
     """
@@ -28,23 +29,18 @@ def main() -> None:
     tokenized_text = tokenize(de_text)
     text_without_stopwords = remove_stop_words(tokenized_text, stopwords)
     calculated_frequencies = calculate_frequencies(text_without_stopwords)
-    result = get_top_n_words(calculated_frequencies, 7)
     unk_profile = create_language_profile("unknown", unknown_text, stopwords)
     de_profile = create_language_profile("de", de_text, stopwords)
     en_profile = create_language_profile("en", en_text, stopwords)
     checks = [check_profile(unk_profile), check_profile(de_profile), check_profile(en_profile)]
     if not all(checks):
         result = None
-    else:
-        result = detect_language_by_top_n(unk_profile, en_profile, de_profile, 15)
-        result = detect_language_by_mse(unk_profile, en_profile, de_profile)
+    print(detect_language_by_top_n(unk_profile, en_profile, de_profile, 15))
+    print(detect_language_by_mse(unk_profile, en_profile, de_profile))
+    result = get_top_n_words(calculated_frequencies, 7)
     assert result, "Detection result is None"
     return result
 
-
-result = main()
-print(result)
-
 if __name__ == "__main__":
-    main()
+    print(main())
 
