8b83ee3a31feb4774ea349c3ee682280108c9f72
drunohhly <nata26032007@gmail.com>
2026-09-25 11:12:55 +0300

    changed start.py and used 'None' explicitly

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -2,7 +2,7 @@
 Language detection starter.
 """
 
-# pylint: disable=unused-variable, duplicate-code
+# pylint: disable=unused-variable, duplicate-code, too-many-return-statements
 from lab_1_classify_profile.main import (
     calculate_frequencies,
     collect_profiles,
@@ -35,15 +35,15 @@ def main() -> None:
 
     tokenized_text = tokenize(de_text)
     if tokenized_text is None:
-        return
+        return None
 
     text_without_stopwords = remove_stop_words(tokenized_text, stopwords)
     if text_without_stopwords is None:
-        return
+        return None
 
     calculated_frequencies = calculate_frequencies(text_without_stopwords)
     if calculated_frequencies is None:
-        return
+        return None
 
     unk_profile = create_language_profile("unknown", unknown_text, stopwords)
     de_profile = create_language_profile("de", de_text, stopwords)
@@ -52,7 +52,7 @@ def main() -> None:
     if (unk_profile is None
         or de_profile is None
         or en_profile is None):
-        return
+        return None
 
     print(get_top_n_words(calculated_frequencies, 7))
     print(detect_language_by_top_n(unk_profile, en_profile, de_profile, 15))
@@ -68,16 +68,18 @@ def main() -> None:
     collected_profiles = collect_profiles(list_of_paths)
 
     if collected_profiles is None:
-        return
+        return None
 
     advanced_detection = detect_language_advanced(unk_profile, collected_profiles, 15)
 
     if advanced_detection is None:
-        return
+        return None
 
     print_report(unk_profile, advanced_detection, 15)
 
+
     assert result, "Detection result is None"
+    return None
 
 if __name__ == "__main__":
     main()
